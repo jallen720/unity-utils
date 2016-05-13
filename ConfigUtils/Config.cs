5eb167cd263949c004feafcfa7dcbc432fbd4d28
Joel Allen <joelallen720@gmail.com>
2016-05-12 17:10:31 -0700

    Add Config.

---
+++ ConfigUtils/Config.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.ConfigUtils {
+    public class Config<T> : ScriptableObject where T : Config<T> {
+        private static T instance;
+
+        protected static T Instance {
+            get {
+                return instance ?? LoadInstance();
+            }
+        }
+
+        private static T LoadInstance() {
+            return instance = Resources.Load<T>(GetConfigPath());
+        }
+
+        protected static string GetConfigPath() {
+            const string RESOURCE_DIRECTORY = "Configs/";
+
+            return RESOURCE_DIRECTORY + typeof(T).ToString();
+        }
+    }
+}
